3a7b413da1bc74cb91f9d05c55703209137ff418
sam <dev@sxlijin.com>
2023-04-02 12:14:37 -0700

    feat: set up add handler, clean up GetHighWatermark

--- 4-counter/handle_add.go
+++ 4-counter/handle_add.go
@@ -5,30 +5,30 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-type TopologyRequest struct {
+type AddRequest struct {
 	MsgId   int64  `json:"msg_id,omitempty"`
 	Type    string `json:"type"`
 	Message int64  `json:"message"`
 }
 
-type TopologyResponse struct {
+type AddResponse struct {
 	InReplyTo int64  `json:"msg_id,omitempty"`
 	Type      string `json:"type"`
 }
 
-func makeTopologyResponse(req *TopologyRequest) TopologyResponse {
-	return TopologyResponse{
+func makeAddResponse(req *AddRequest) AddResponse {
+	return AddResponse{
 		InReplyTo: req.MsgId,
-		Type:      "topology_ok",
+		Type:      "add_ok",
 	}
 }
 
-func (ls *LocalStore) HandleTopology(msg maelstrom.Message) error {
-	var req TopologyRequest
+func (ls *LocalStore) HandleAdd(msg maelstrom.Message) error {
+	var req AddRequest
 	if err := json.Unmarshal(msg.Body, &req); err != nil {
 		return err
 	}
-	resp := makeTopologyResponse(&req)
+	resp := makeAddResponse(&req)
 
 	return ls.n.Reply(msg, resp)
 }
--- 4-counter/main.go
+++ 4-counter/main.go
@@ -34,12 +34,8 @@ func (ls *LocalStore) Run() error {
 	})
 
 	// Maelstrom test handlers
-	ls.n.Handle("broadcast", ls.HandleBroadcast)
+	ls.n.Handle("add", ls.HandleAdd)
 	ls.n.Handle("read", ls.HandleRead)
-	ls.n.Handle("topology", ls.HandleTopology)
-
-	// Custom handlers
-	ls.n.Handle("GetHighWatermark", ls.HandleGetHighWatermark)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	go ls.ManagePollers(ctx)
@@ -109,34 +105,6 @@ func (ls *LocalStore) PollNode(pctx context.Context, nodeId string) {
 
 func (ls *LocalStore) PollNeighbors(ctx context.Context) {
 	ls.l.Printf("Attempting to poll neighbors")
-}
-
-// TODO: implement described optimization
-// If LWM == HWM, returns empty values slice
-// If LWM != HWM, returns all values in local DB - cannot know which values client doesn't know about
-func (ls *LocalStore) HandleGetHighWatermark(req maelstrom.Message) error {
-	var reqBody struct {
-		MsgId int64  `json:"msg_id"`
-		Type  string `json:"type"`
-	}
-	if err := json.Unmarshal(req.Body, &reqBody); err != nil {
-		return err
-	}
-	respBody := make(map[string]any)
-
-	ls.dbMu.Lock()
-	defer ls.dbMu.Unlock()
-
-	c := make([]int64, 0)
-	for val, _ := range ls.db {
-		c = append(c, val)
-	}
-	respBody["in_reply_to"] = reqBody.MsgId
-	respBody["type"] = "GetHighWatermark_ok"
-	respBody["high_watermark"] = len(ls.db)
-	respBody["db"] = c
-
-	return ls.n.Reply(req, respBody)
 }
 
 func main() {
